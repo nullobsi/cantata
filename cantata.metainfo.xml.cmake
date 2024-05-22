7feeea07fda211bfed619ee20b9f7475d2057bae
Citlali del Rey <nullobsi@unix.dog>
2024-05-22 12:30:36 -0700

    build: add metainfo

---
+++ cantata.metainfo.xml.cmake
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<component type="desktop-application">
+  <id>@PROJECT_REV_ID@</id>
+  
+  <name>Cantata</name>
+  <summary>Graphical MPD client made with Qt.</summary>
+  
+  <metadata_license>CC-BY-SA-4.0</metadata_license>
+  <project_license>GPL-3.0-or-later</project_license>
+  
+  <description>
+    <p>
+      Control your MPD server with ease and efficiency.
+    </p>
+  </description>
+  
+  <launchable type="desktop-id">@PROJECT_REV_ID@.desktop</launchable>
+</component>
