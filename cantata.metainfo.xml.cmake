0b82b6962313a4a12fae8cb7c1c013b19429801e
Citlali del Rey <nullobsi@unix.dog>
2024-05-30 16:01:32 -0700

    git: update screenshots in metainfo

--- cantata.metainfo.xml.cmake
+++ cantata.metainfo.xml.cmake
@@ -35,18 +35,17 @@
 
   <url type="homepage">https://cantata.unix.dog/</url>
   <url type="vcs-browser">https://cantata.unix.dog/src</url>
-
   <screenshots>
     <screenshot type="default">
-	  <image>https://raw.githubusercontent.com/nullobsi/cantata/5e991803b8e4b4058419c647f2cfcd3e81273bde/screenshots/mainwindow.png</image>
+	  <image>https://raw.githubusercontent.com/nullobsi/cantata/3198a3362c3d5d7425eea4f50043ba3198b75723/screenshots/mainwindow.png</image>
 	  <caption>A playlist is open on the right, with some albums to add on the left.</caption>
 	</screenshot>
     <screenshot>
-	  <image>https://raw.githubusercontent.com/nullobsi/cantata/5e991803b8e4b4058419c647f2cfcd3e81273bde/screenshots/artist1.png</image>
+	  <image>https://raw.githubusercontent.com/nullobsi/cantata/3198a3362c3d5d7425eea4f50043ba3198b75723/screenshots/artist1.png</image>
 	  <caption>The information pane is open displaying an artist's biography and the current song lyrics.</caption>
 	</screenshot>
     <screenshot>
-	  <image>https://raw.githubusercontent.com/nullobsi/cantata/5e991803b8e4b4058419c647f2cfcd3e81273bde/screenshots/artist2.png</image>
+	  <image>https://raw.githubusercontent.com/nullobsi/cantata/3198a3362c3d5d7425eea4f50043ba3198b75723/screenshots/artist2.png</image>
 	  <caption>The information pane is open, showing related songs and albums.</caption>
 	</screenshot>
   </screenshots>
