nullobsi/cantata

commit 8e7f8ef4c37c7c9873be3681b65f385892f744b0

build: modernize FindMusicBrainz5.cmake

cmake/FindMusicBrainz5.cmake
@@ -1,40 +1,86 @@
-# Module to find the musicbrainz-4 library
-#
-# It defines
-#  MUSICBRAINZ5_INCLUDE_DIRS - the include dir
-#  MUSICBRAINZ5_LIBRARIES - the required libraries
-#  MUSICBRAINZ5_FOUND - true if both of the above have been found
+#[===[.rst:
+FindMusicBrainz5
+----------------
 
-# Copyright (c) 2006,2007 Laurent Montel, <montel@kde.org>
-#
-# Redistribution and use is allowed according to the terms of the BSD license.
-# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
+Finds the MusicBrainz Libraries.
 
-if(MUSICBRAINZ5_INCLUDE_DIRS AND MUSICBRAINZ5_LIBRARIES)
-   set(MUSICBRAINZ5_FIND_QUIETLY TRUE)
-endif(MUSICBRAINZ5_INCLUDE_DIRS AND MUSICBRAINZ5_LIBRARIES)
+Imported Targets
+^^^^^^^^^^^^^^^^
 
-IF (NOT WIN32)
-   find_package(PkgConfig)
+This module provides the following imported targets, if found:
 
-   PKG_SEARCH_MODULE( MUSICBRAINZ5 libmusicbrainz5cc )
-   if (NOT MUSICBRAINZ5_FOUND)
-      PKG_SEARCH_MODULE( MUSICBRAINZ5 libmusicbrainz5 )
-   endif (NOT MUSICBRAINZ5_FOUND)
+``MusicBrainz5::MusicBrainz``
+  The MusicBrainz library.
 
-ELSE (NOT WIN32)
-  FIND_PATH( MUSICBRAINZ5_INCLUDE_DIRS musicbrainz5/Disc.h )
+Result Variables
+^^^^^^^^^^^^^^^^
 
-  FIND_LIBRARY( MUSICBRAINZ5_LIBRARIES NAMES musicbrainz5cc )
-  if (NOT MUSICBRAINZ5_LIBRARIES)
-     FIND_LIBRARY( MUSICBRAINZ5_LIBRARIES NAMES musicbrainz5 )
-  endif (NOT MUSICBRAINZ5_LIBRARIES)
+This will define the following variables:
 
-ENDIF (NOT WIN32)
+``MusicBrainz5_FOUND``
+  True if the system has the MusicBrainz libraries.
+``MusicBrainz5_VERSION``
+  The version of MusicBrainz which was found.
+``MusicBrainz5_INCLUDE_DIRS``
+  Include directories needed to use MusicBrainz.
+``MusicBrainz5_LIBRARIES``
+  Libraries needed to link to MusicBrainz.
+
+Cache Variables
+^^^^^^^^^^^^^^^
+
+The following cache variables may also be set:
+
+``MusicBrainz5_LIBRARY``
+  The path to the MusicBrainz library.
+``MusicBrainz5_INCLUDE_DIR``
+  The directory containing ``Disc.h``.
+#]===]
+
+# First use PKG-Config as a starting point.
+find_package(PkgConfig)
+if(PKG_CONFIG_FOUND)
+    pkg_check_modules(PC_MusicBrainz5 QUIET libmusicbrainz5)
+endif(PKG_CONFIG_FOUND)
+
+find_path(MusicBrainz5_INCLUDE_DIR
+    NAMES Disc.h
+    PATHS ${PC_MusicBrainz5_INCLUDE_DIRS}
+    PATH_SUFFIXES "musicbrainz5"
+)
+
+find_library(MusicBrainz5_LIBRARY
+    NAMES musicbrainz5
+    PATHS ${PC_MusicBrainz5_LIBRARY_DIRS}
+)
+
+# Set version from PC if applicable.
+set(MusicBrainz5_VERSION ${PC_MusicBrainz5_VERSION})
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args( MUSICBRAINZ5 DEFAULT_MSG
-                                   MUSICBRAINZ5_INCLUDE_DIRS MUSICBRAINZ5_LIBRARIES)
+find_package_handle_standard_args(MusicBrainz5
+    FOUND_VAR MusicBrainz5_FOUND
+    REQUIRED_VARS
+        MusicBrainz5_INCLUDE_DIR
+        MusicBrainz5_LIBRARY
+    VERSION_VAR MusicBrainz5_VERSION
+)
 
-MARK_AS_ADVANCED(MUSICBRAINZ5_INCLUDE_DIRS MUSICBRAINZ5_LIBRARIES)
+if(MusicBrainz5_FOUND)
+    set(MusicBrainz5_LIBRARIES ${MusicBrainz5_LIBRARY})
+    set(MusicBrainz5_INCLUDE_DIRS ${MusicBrainz5_INCLUDE_DIR})
+    set(MusicBrainz5_DEFINITIONS ${PC_MusicBrainz5_CFLAGS_OTHER})
+    if(NOT TARGET MusicBrainz5::MusicBrainz)
+        add_library(MusicBrainz5::MusicBrainz UNKNOWN IMPORTED)
+        set_target_properties(MusicBrainz5::MusicBrainz PROPERTIES
+            IMPORTED_LOCATION "${MusicBrainz5_LIBRARY}"
+            INTERFACE_COMPILE_OPTIONS "${PC_MusicBrainz5_CFLAGS_OTHER}"
+            INTERFACE_INCLUDE_DIRECTORIES "${MusicBrainz5_INCLUDE_DIR}"
+        )
+    endif ()
+endif()
 
+mark_as_advanced(
+        MusicBrainz5_INCLUDE_DIR
+        MusicBrainz5_LIBRARY
+)

CMakeLists.txt
@@ -235,7 +235,7 @@ if(TagLib_FOUND)
             endif()
             if(ENABLE_MUSICBRAINZ)
                 find_package(MusicBrainz5)
-                macro_log_feature(MUSICBRAINZ5_FOUND "libmusicbrainz5" "CD info retrieval via MusicBrainz." "http://musicbrainz.org/doc/libmusicbrainz")
+                macro_log_feature(MusicBrainz5_FOUND "libmusicbrainz5" "CD info retrieval via MusicBrainz." "http://musicbrainz.org/doc/libmusicbrainz")
             endif()
         endif()
     endif()
@@ -360,20 +360,6 @@ list(
     PREPEND Qt${QT_VERSION_MAJOR}::
     OUTPUT_VARIABLE QT_LIBS
 )
-
-# MusicBrainz requires exceptions to be enabled!
-if(MUSICBRAINZ5_FOUND)
-    message("-- Enabling exceptions")
-    if(CMAKE_COMPILER_IS_GNUCXX)
-        add_definitions("-fexceptions -UQT_NO_EXCEPTIONS")
-    endif(CMAKE_COMPILER_IS_GNUCXX)
-    if(CMAKE_C_COMPILER MATCHES "icc")
-        add_definitions(-fexceptions)
-    endif()
-    if(MSVC)
-        add_definitions(-EHsc)
-    endif()
-endif()
 
 include_directories(
     ${CMAKE_SOURCE_DIR}/3rdparty
@@ -630,15 +616,14 @@ if(TagLib_FOUND)
             set(CANTATA_SRCS ${CANTATA_SRCS} devices/mtpdevice.cpp)
             set(CANTATA_LIBS ${CANTATA_LIBS} MTP::MTP)
         endif()
-        if(CDDB_FOUND OR MUSICBRAINZ5_FOUND)
+        if(CDDB_FOUND OR MusicBrainz5_FOUND)
             if(CDDB_FOUND)
                 set(CANTATA_SRCS ${CANTATA_SRCS} devices/cddbinterface.cpp)
                 set(CANTATA_LIBS ${CANTATA_LIBS} CDDB::CDDB)
             endif()
-            if(MUSICBRAINZ5_FOUND)
+            if(MusicBrainz5_FOUND)
                 set(CANTATA_SRCS ${CANTATA_SRCS} devices/musicbrainz.cpp)
-                include_directories(${MUSICBRAINZ5_INCLUDE_DIRS})
-                set(CANTATA_LIBS ${CANTATA_LIBS} ${MUSICBRAINZ5_LIBRARIES})
+                set(CANTATA_LIBS ${CANTATA_LIBS} MusicBrainz5::MusicBrainz)
             endif()
             set(CANTATA_SRCS
                 ${CANTATA_SRCS}

config.h.cmake
@@ -28,7 +28,7 @@
 #cmakedefine FFMPEG_FOUND 1
 #cmakedefine MPG123_FOUND 1
 #cmakedefine CDDB_FOUND 1
-#cmakedefine MUSICBRAINZ5_FOUND 1
+#cmakedefine MusicBrainz5_FOUND 1
 #cmakedefine ENABLE_REPLAYGAIN_SUPPORT 1
 #cmakedefine ENABLE_PROXY_CONFIG 1
 #cmakedefine CDPARANOIA_HAS_CACHEMODEL_SIZE 1

devices/audiocddevice.cpp
@@ -25,7 +25,7 @@
 #ifdef CDDB_FOUND
 #include "cddbinterface.h"
 #endif
-#ifdef MUSICBRAINZ5_FOUND
+#ifdef MusicBrainz5_FOUND
 #include "musicbrainz.h"
 #endif
 #include "models/musiclibraryitemsong.h"
@@ -80,7 +80,7 @@ AudioCdDevice::AudioCdDevice(MusicLibraryModel *m, Solid::Device &dev)
     #ifdef CDDB_FOUND
     , cddb(0)
     #endif
-    #ifdef MUSICBRAINZ5_FOUND
+    #ifdef MusicBrainz5_FOUND
     , mb(0)
     #endif
     , year(0)
@@ -111,7 +111,7 @@ AudioCdDevice::AudioCdDevice(MusicLibraryModel *m, Solid::Device &dev)
             registeredTypes=true;
         }
         devPath=Song::constCddaProtocol+device+QChar('/');
-        #if defined CDDB_FOUND && defined MUSICBRAINZ5_FOUND
+        #if defined CDDB_FOUND && defined MusicBrainz5_FOUND
         connectService(Settings::self()->useCddb());
         #else
         connectService(true);
@@ -133,7 +133,7 @@ AudioCdDevice::~AudioCdDevice()
         cddb=0;
     }
     #endif
-    #ifdef MUSICBRAINZ5_FOUND
+    #ifdef MusicBrainz5_FOUND
     if (mb) {
         mb->deleteLater();
         mb=0;
@@ -168,7 +168,7 @@ bool AudioCdDevice::isAudioDevice(const QString &dev) const
 
 void AudioCdDevice::connectService(bool useCddb)
 {
-    #if defined CDDB_FOUND && defined MUSICBRAINZ5_FOUND
+    #if defined CDDB_FOUND && defined MusicBrainz5_FOUND
     if (cddb && !useCddb) {
         cddb->deleteLater();
         cddb=0;
@@ -183,7 +183,7 @@ void AudioCdDevice::connectService(bool useCddb)
 
     #ifdef CDDB_FOUND
     if (!cddb
-            #ifdef MUSICBRAINZ5_FOUND
+            #ifdef MusicBrainz5_FOUND
             && useCddb
             #endif
             ) {
@@ -195,7 +195,7 @@ void AudioCdDevice::connectService(bool useCddb)
     }
     #endif
 
-    #ifdef MUSICBRAINZ5_FOUND
+    #ifdef MusicBrainz5_FOUND
     if (!mb
             #ifdef CDDB_FOUND
             && !useCddb

devices/audiocddevice.h
@@ -108,7 +108,7 @@ private:
     #ifdef CDDB_FOUND
     CddbInterface *cddb;
     #endif
-    #ifdef MUSICBRAINZ5_FOUND
+    #ifdef MusicBrainz5_FOUND
     MusicBrainz *mb;
     #endif
     QString detailsString;

devices/audiocdsettings.cpp
@@ -33,7 +33,7 @@ AudioCdSettings::AudioCdSettings(QWidget *p)
     : QWidget(p)
 {
     setupUi(this);
-    #if defined CDDB_FOUND && defined MUSICBRAINZ5_FOUND
+    #if defined CDDB_FOUND && defined MusicBrainz5_FOUND
     cdLookup->addItem(tr("CDDB"), true);
     cdLookup->addItem(tr("MusicBrainz"), false);
     #else
@@ -58,7 +58,7 @@ void AudioCdSettings::load()
     #endif
     paranoiaFull->setChecked(Settings::self()->paranoiaFull());
     paranoiaNeverSkip->setChecked(Settings::self()->paranoiaNeverSkip());
-    #if defined CDDB_FOUND && defined MUSICBRAINZ5_FOUND
+    #if defined CDDB_FOUND && defined MusicBrainz5_FOUND
     for (int i=0; i<cdLookup->count(); ++i) {
         if (cdLookup->itemData(i).toBool()==Settings::self()->useCddb()) {
             cdLookup->setCurrentIndex(i);
@@ -78,7 +78,7 @@ void AudioCdSettings::save()
     #endif
     Settings::self()->saveParanoiaFull(paranoiaFull->isChecked());
     Settings::self()->saveParanoiaNeverSkip(paranoiaNeverSkip->isChecked());
-    #if defined CDDB_FOUND && defined MUSICBRAINZ5_FOUND
+    #if defined CDDB_FOUND && defined MusicBrainz5_FOUND
     Settings::self()->saveUseCddb(cdLookup->itemData(cdLookup->currentIndex()).toBool());
     #endif
     Settings::self()->saveParanoiaOffset(paranoiaOffset->value());

devices/device.cpp
@@ -36,9 +36,9 @@
 #ifdef MTP_FOUND
 #include "mtpdevice.h"
 #endif // MTP_FOUND
-#if defined CDDB_FOUND || defined MUSICBRAINZ5_FOUND
+#if defined CDDB_FOUND || defined MusicBrainz5_FOUND
 #include "audiocddevice.h"
-#endif // defined CDDB_FOUND || defined MUSICBRAINZ5_FOUND
+#endif // defined CDDB_FOUND || defined MusicBrainz5_FOUND
 #include "encoders.h"
 #include "tags/tags.h"
 #include "mpd-interface/song.h"
@@ -146,7 +146,7 @@ Device * Device::create(MusicLibraryModel *m, const QString &udi)
 
     DBUG_CLASS("Device") << udi;
     if (device.is<Solid::OpticalDisc>()) {
-        #if defined CDDB_FOUND || defined MUSICBRAINZ5_FOUND
+        #if defined CDDB_FOUND || defined MusicBrainz5_FOUND
         DBUG_CLASS("Device") << "AudioCD";
         return Encoders::getAvailable().isEmpty() ? 0 : new AudioCdDevice(m, device);
         #endif

devices/devicespage.cpp
@@ -49,7 +49,7 @@
 #include "gui/preferencesdialog.h"
 #include "gui/coverdialog.h"
 #include "mpd-interface/mpdconnection.h"
-#if defined CDDB_FOUND || defined MUSICBRAINZ5_FOUND
+#if defined CDDB_FOUND || defined MusicBrainz5_FOUND
 #include "audiocddevice.h"
 #include "albumdetailsdialog.h"
 #include "cddbselectiondialog.h"
@@ -78,7 +78,7 @@ DevicesPage::DevicesPage(QWidget *p)
     connect(copyAction, SIGNAL(triggered()), this, SLOT(copyToLibrary()));
     connect(DevicesModel::self()->configureAct(), SIGNAL(triggered()), this, SLOT(configureDevice()));
     connect(DevicesModel::self()->refreshAct(), SIGNAL(triggered()), this, SLOT(refreshDevice()));
-    #if defined CDDB_FOUND || defined MUSICBRAINZ5_FOUND
+    #if defined CDDB_FOUND || defined MusicBrainz5_FOUND
     connect(DevicesModel::self()->editAct(), SIGNAL(triggered()), this, SLOT(editDetails()));
     connect(DevicesModel::self(), SIGNAL(matches(const QString &, const QList<CdAlbum> &)),
             SLOT(cdMatches(const QString &, const QList<CdAlbum> &)));
@@ -334,7 +334,7 @@ void DevicesPage::controlActions()
     #ifdef ENABLE_REMOTE_DEVICES
     forgetDeviceAction->setEnabled(singleUdi && remoteDev);
     #endif
-    #if defined CDDB_FOUND || defined MUSICBRAINZ5_FOUND
+    #if defined CDDB_FOUND || defined MusicBrainz5_FOUND
     DevicesModel::self()->editAct()->setEnabled(!AlbumDetailsDialog::instanceCount() && !busyDevice && 1==selected.count() && audioCd && haveTracks && deviceSelected);
     #endif
 }
@@ -405,7 +405,7 @@ void DevicesPage::refreshDevice()
 
         if (Device::AudioCd==dev->devType()) {
             // Bit hacky - we use 'full' to determine CDDB/MusicBrainz!
-            #if defined CDDB_FOUND && defined MUSICBRAINZ5_FOUND
+            #if defined CDDB_FOUND && defined MusicBrainz5_FOUND
             switch (MessageBox::questionYesNoCancel(this, tr("Lookup album and track details?"),
                                                     tr("Refresh"), GuiItem(tr("Via CDDB")), GuiItem(tr("Via MusicBrainz")))) {
             case MessageBox::Yes:
@@ -571,7 +571,7 @@ void DevicesPage::updated(const QModelIndex &idx)
 
 void DevicesPage::cdMatches(const QString &udi, const QList<CdAlbum> &albums)
 {
-    #if defined CDDB_FOUND || defined MUSICBRAINZ5_FOUND
+    #if defined CDDB_FOUND || defined MusicBrainz5_FOUND
     int chosen=0;
     Device *dev=DevicesModel::self()->device(udi);
     if (dev && Device::AudioCd==dev->devType()) {
@@ -595,7 +595,7 @@ void DevicesPage::cdMatches(const QString &udi, const QList<CdAlbum> &albums)
 
 void DevicesPage::editDetails()
 {
-    #if defined CDDB_FOUND || defined MUSICBRAINZ5_FOUND
+    #if defined CDDB_FOUND || defined MusicBrainz5_FOUND
     if (AlbumDetailsDialog::instanceCount()) {
         return;
     }

commit a6d9bfadd079d856b36d56aeb14e6cb7aa27b0a2

build: Link to libmusicbrainzcc, not libmusicbrainz
Fix linking when the ENABLE_MUSICBRAINZ option is true. This was
regressed in 8e7f8ef, by linking to the libmusicbrainz (C) library
instead of the libmusicbrainzcc (C++) library causing undefined symbols.

cmake/FindMusicBrainz5.cmake
@@ -40,7 +40,7 @@ The following cache variables may also be set:
 # First use PKG-Config as a starting point.
 find_package(PkgConfig)
 if(PKG_CONFIG_FOUND)
-    pkg_check_modules(PC_MusicBrainz5 QUIET libmusicbrainz5)
+    pkg_check_modules(PC_MusicBrainz5 QUIET libmusicbrainz5cc)
 endif(PKG_CONFIG_FOUND)
 
 find_path(MusicBrainz5_INCLUDE_DIR
@@ -50,7 +50,7 @@ find_path(MusicBrainz5_INCLUDE_DIR
 )
 
 find_library(MusicBrainz5_LIBRARY
-    NAMES musicbrainz5
+    NAMES musicbrainz5cc
     PATHS ${PC_MusicBrainz5_LIBRARY_DIRS}
 )
 