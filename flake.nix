27580b26ee8dbcd90014c9fe78b88a52458767fb
Citlali del Rey <nullobsi@unix.dog>
2025-04-23 20:58:39 -0700

    git: add package to flake

--- flake.nix
+++ flake.nix
@@ -5,9 +5,12 @@
 	};
 	outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
 		let
+			name = "cantata";
+			version = "3.3.1";
 			pkgs = import nixpkgs {
 				inherit system;
 			};
+			isLinux = pkgs.lib.strings.hasPrefix "-linux" system;
 			qtEnv = with pkgs.qt6; env "qt-custom-${qtbase.version}" 
 				[
 					qtconnectivity
@@ -18,20 +21,35 @@
 					qttranslations
 					qttools
 				];
+			buildInputs = with pkgs; [
+				taglib
+				ffmpeg
+				mpg123
+				libebur128
+				avahi
+				zlib
+				qtEnv
+				cmake
+				ninja
+			] ++ lib.optionals isLinux [
+				cdio-paranoia
+				libmusicbrainz5
+				libmtp
+				media-player-info
+			];
 		in
 		{
+			packages.default =
+				let
+					inherit (pkgs) stdenv lib;
+				in
+				stdenv.mkDerivation {
+					inherit version buildInputs;
+					src = self;
+					pname = name;
+				};
 			devShells.default = pkgs.mkShell {
-				buildInputs = with pkgs; [
-					taglib
-					ffmpeg
-					mpg123
-					libebur128
-					avahi
-					zlib
-					qtEnv
-					cmake
-					ninja
-				];
+				inherit buildInputs;
 			};
 		}
 	);
